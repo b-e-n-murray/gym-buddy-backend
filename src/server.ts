7cf525c3f3a4a1564d59775e01fe3a0616eca0df
Ben Murray <benmurray573@gmail.com>
2023-01-24 10:43:23 +0000

    added limit dependent on difficulty, changed endpoint

--- src/server.ts
+++ src/server.ts
@@ -14,15 +14,32 @@ client.connect();
 
 
 // GET /items/:id
-app.get("/:targetMuscles", async (req, res) => {
+app.get("/exercises/:targetMuscles/:difficulty", async (req, res) => {
   try {
+    function determineLimit(targetArray: string, difficulty: string): number | undefined {
+      if (difficulty === 'Easy') {
+        return targetArray.length === 1 ? 2 : 3
+      }
+      if (difficulty === 'Intermediate') {
+        return targetArray.length === 1 ? 2 : 4
+      }
+      if (difficulty === 'Hard') {
+        return targetArray.length === 1 ? 3 : 5
+      }
+      else return undefined
+    }
     const targetMuscles = req.params.targetMuscles;
+    const difficulty = req.params.difficulty
+    const limit = determineLimit(targetMuscles, difficulty)
     const separatedStrings = targetMuscles.split(",");
     console.log("retrieving exercises targeting: ", separatedStrings);
     const relevantExercises = await client.query(
-      "SELECT * FROM exercise_data WHERE targets @> $1::VARCHAR[] OR targets && $1::VARCHAR[] LIMIT 5;",
+      `SELECT * FROM exercise_data 
+      WHERE targets @> $1::VARCHAR[] 
+      OR targets && $1::VARCHAR[] 
+      LIMIT $2;`,
       //select all rows where the targets column contains at least one of the elements in the given array.
-      [separatedStrings]
+      [separatedStrings, limit]
     );
     res.status(200).json(relevantExercises.rows);
   }
